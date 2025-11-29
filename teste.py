628c0df22b3a5c6396adac248723c436e46d3283
Emerson S Motta <rgorosp@hotmail.com>
2025-11-29 08:00:02 -0300

    Novo Programa de Teste

---
+++ teste.py
@@ -0,0 +1,16 @@
+# Programa: teste.py
+"""
+Exemplos de Comandos
+"""
+index = 0
+
+nome = str(input("Digite o seu Nome: "))
+
+while index < len(nome):
+    letra = nome[index]
+    print(letra)
+    index += 1
+    
+print("Fim do Programa")
+
+
