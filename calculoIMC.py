a56230e57f056384568785f0391398cee9a19d62
Emerson S Motta <rgorosp@hotmail.com>
2026-04-20 17:10:42 -0300

    Atualização

--- calculoIMC.py
+++ calculoIMC.py
@@ -1,5 +1,7 @@
 # Programa: calculoIMC.py
 # Cálculo do Índice de Massa Corporal (IMC)
+import os
+
 peso = float(input("Digite seu peso em kg: "))  # 
 altura = float(input("Digite a sua altura: "))  # 
 
@@ -7,12 +9,25 @@ altura = float(input("Digite a sua altura: "))  #
 imc = peso / (altura ** 2)
 print(f"Seu IMC é: {imc:.2f}")
 
+# PROCESSAMENTO
 """Classifica o IMC de acordo com os padrões da OMS """
-if imc < 18.5:
-    print("Abaixo do peso")
-elif 18.5 <= imc < 24.9:
-    print("Peso normal")
-elif 25 <= imc < 29.9:
-    print("Sobrepeso")
-else:
-    print("Obesidade")
+def processamento():
+    if imc < 18.5:
+        print("Abaixo do peso")
+    elif 18.5 <= imc < 24.9:
+        print("Peso normal")
+    elif 25 <= imc < 29.9:
+        print("Sobrepeso")
+    else:
+        print("Obesidade")
+
+    print()
+
+# INICIO DO PROGRAMA
+def main():
+    os.system('cls')
+
+    processamento()
+
+if __name__ == "__main__":
+    main()
--- operadoresAritmeticos.py
+++ operadoresAritmeticos.py
@@ -1,4 +1,6 @@
 # operadores aritméticos (matemática) usando Strings Formatada
+import os
+
 adicao = 10 + 10
 subtracao = 10 - 5
 multiplicacao = 10 * 10
@@ -7,10 +9,22 @@ divisao_inteira = 10 // 3
 modulo = 10 % 3
 exponenciacao = 10 ** 2
 
-print(f'Adição: {adicao}')
-print(f'Subtração: {subtracao}')
-print(f'Multiplicação: {multiplicacao}')
-print(f'Divisão: {divisao}')
-print(f'Divisão inteira: {divisao_inteira}')
-print(f'Módulo: {modulo}')
-print(f'Exponenciação: {exponenciacao}')
+# PROCESSAMENTO
+def processamento():
+
+    print(f'Adicao: {adicao}')
+    print(f'Subtracao: {subtracao}')
+    print(f'Multiplicacao: {multiplicacao}')
+    print(f'Divisao: {divisao:.2f}')
+    print(f'Divisao inteira: {divisao_inteira}')
+    print(f'Modulo: {modulo}')
+    print(f'Exponenciacao: {exponenciacao}')
+    print()
+
+# INICIO DO PROGRAMA
+def main():
+    os.system('cls')
+    processamento()
+
+if __name__ == "__main__":
+    main()
